d17f39d268bd4a455511dac3fb3db0ebbaee7dd0
river666styx <rstyx666@gmail.com>
2024-09-11 17:39:39 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'MBIS'
+copyright = '2024, River Styx'
+author = 'River Styx'
 
 release = '0.1'
 version = '0.1.0'
